8ca6808a81deeef9049d26697f9a5c3b50f9b528
Jan Dankert <Jan Dankert@localhost>
2012-02-06 22:02:19 +0100

    Receiver f?r den Netzwerk-Status.

---
+++ src/de/mtbnews/android/receiver/NetworkStateReceiver.java
@@ -0,0 +1,30 @@
+package de.mtbnews.android.receiver;
+
+import de.mtbnews.android.service.SubscriptionService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+public class NetworkStateReceiver extends BroadcastReceiver
+{
+
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		boolean noConnectivity = intent.getBooleanExtra(
+				ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+		if (!noConnectivity)
+		{
+			Log.d("IBC", "connection lost, stopping service");
+			context.stopService(new Intent(context, SubscriptionService.class));
+		}
+		else
+		{
+			Log.d("IBC", "connection established, starting service");
+			context
+					.startService(new Intent(context, SubscriptionService.class));
+		}
+	}
+}
